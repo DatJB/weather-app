98a2e78ff90e594cf97fb71506ae01b3b0c48a5b
Dat <nguyendinhdat343@gmail.com>
2026-02-06 02:04:45 +0700

    images cache

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["assets/images/*.svg"],
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+      },
+    }),
+  ],
   base: '/weather-app/',
 })
